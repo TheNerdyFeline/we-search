c0bffd650c4053c37b67d076fb467aea15baf17c
Jared Neutel <Jared@Macintosh-2.local>
2017-07-01 20:08:47 -0700

    added pubcounter component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormInstance from './FormInstance';
 import Mission from '../Mission/Mission';
+import PubCounter from '../PubCounter/PubCounter';
 
 export default class Login extends Component {
 	render() {
@@ -8,6 +9,7 @@ export default class Login extends Component {
 			<div>
 				<FormInstance />
 				<Mission />
+				<PubCounter />
 			</div>
 		)
 	}	
