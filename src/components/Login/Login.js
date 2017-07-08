d03b4d8dd8230660f9de23459fc9313ebc25c454
Jackie Wellons <jackie@jackie-dell.jackie>
2017-07-08 11:11:16 -0700

    navbar loads, but needs to be loaded into each component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+// import children components
+import NavbarComponent from '../Nav/NavbarComponent';
 import FormInstance from './FormInstance';
 import Mission from '../Mission/Mission';
 import PubCounter from '../PubCounter/PubCounter';
@@ -7,10 +10,11 @@ export default class Login extends Component {
 	render() {
 		return (
 			<div>
-				<FormInstance />
-				<Mission />
-				<PubCounter />
+			  <NavbarComponent/>
+			  <FormInstance />
+			  <Mission />
+			  <PubCounter />
 			</div>
-		)
+		);
 	}	
 };
