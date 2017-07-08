f7b99e3cce627bd1827110f773d171eb85e473bc
Jackie Wellons <jackie@jackie-dell.jackie>
2017-07-08 11:04:38 -0700

    routes work, but wont load navbar on all pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM, {render} from 'react-dom';
-import Login from './components/Login/Login.js';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+//import components to render
+import Login from './components/Login/Login';
 import Questions from './components/Questions/Questions';
-//import AppCss from './App.css';
+import AppCss from './App.css';
 import NavbarComponent from './components/Nav/NavbarComponent';
 
-
-class App extends Component {
-  render() {
-    return (
-	<div className="App">
-	     hello
-	      {/* <NavbarComponent />*/}
-	    </div> 
-    );
-  }
-}
+const App = () => (
+    <Router>
+      
+      <Switch>
+      <Route exact path="/" component={Login} />
+      <Route path="/" component={Questions} />
+    </Switch>
+  </Router>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,33 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM, {render} from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
-//import routes from "./routes/routes";
+//import registerServiceWorker from './registerServiceWorker';
 import {Router} from 'react-router';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 
 // import each parent component
 import App from './App';
-import Questions from './components/Questions/Questions';
-import Login from './components/Login/Login';
-// ReactDOM.render(
-// <BrowserRouter>
-// 	<App />
-// </BrowserRouter>
-// ), document.getElementById('root');
-// registerServiceWorker();
+//import Questions from './components/Questions/Questions';
+//import Login from './components/Login/Login';
 
-ReactDOM.render ((
-    <BrowserRouter>
-      <div>
-	{/* this wraps all routes to include navbar */}
-	<Route path="/" Component={App}>
-	  <Route exact path="/" Component={App}/>
-	  <Route path="/login" Component={Login}/>
-	  <Route path="/questions" Component={Questions}/>
-	  </Route>
-      </div>
-    </BrowserRouter>), document.getElementById('root')
-		);
 
-//ReactDOM.render(routes, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(<App />, document.getElementById('root'));
