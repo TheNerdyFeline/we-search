b178487ed982aabb4fa7a5ef226cfec978bf1954
Jackie Wellons <jackie@jackie-dell.jackie>
2017-07-17 21:32:04 -0700

    save changes

--- we-search-db/config/passport.js
+++ we-search-db/config/passport.js
@@ -7,11 +7,11 @@ var db = require("../models");
 passport.use(new LocalStrategy(
   // Our user will sign in using an email
   {
-    usernameField: "email"
+    email: "email"
   },
   function(email, password, done) {
     // When a user tries to sign in this code runs
-    db.User.findOne({
+    db.Prof.findOne({
       where: {
         email: email
       }
